530b00228661c06b5398dffd028b3657bcfafcaf
Chanakya Devraj <cdevraj@grepruby.com>
2018-06-20 17:05:13 +0530

    added find queries

---
+++ node-todo-api/playground/mongoose-queries.js
@@ -0,0 +1,31 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const{Todo} = require('./../server/models/todo');
+
+// var id = '5b2a3845a2e1f3778a0e36cadf';
+
+
+// if(!ObjectID.isValid(id)) {
+//   console.log('ID is not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log(`todos: ${todos}`);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log(`todo: ${todo}`);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log(`todo by id: ${todo}`);
+// }).catch((e) => {
+//   console.log(e);
+// })
