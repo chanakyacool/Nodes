20a4d23f77b8c13411433d3b52a6e27dad844070
Chanakya Devraj <cdevraj@grepruby.com>
2018-06-28 16:25:03 +0530

    added config

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,5 @@
+ require('./config/config.js');
+
 const _ = require('lodash');
 
 const express = require('express');
@@ -10,7 +12,7 @@ const {User} = require('./models/user');
 
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(bodyParser.json());
 
