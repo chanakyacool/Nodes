4a89aa3bbdcb42245685577609e18128a1156300
Chanakya Devraj <cdevraj@grepruby.com>
2018-07-02 12:29:57 +0530

    hashed user password before saving

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,16 +1,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$Qk5LF9r6SAldfCAI9LMvhukzI/LOUHL1tha/iYCpBnIfb8gR.jCvW';
+
+bcrypt.compare(password, hashedPassword, (err, result) =>{
+  // result => true/false
+
+  console.log(result);
+})
 
 
-var data = {
-  id: 10
-}
 
-var token = jwt.sign(data, 'somesecret');
-console.log(token);
-var decoded = jwt.verify(token, 'somesecret');
 
-console.log(decoded);
+// var data = {
+//   id: 10
+// }
+
+// var token = jwt.sign(data, 'somesecret');
+// console.log(token);
+// var decoded = jwt.verify(token, 'somesecret');
+
+// console.log(decoded);
 // var message = 'o adff'
 
 // var hash = SHA256(message).toString();
