cb254eacd5c1e4236625fa008b053c0a708c7f2a
Chanakya Devraj <cdevraj@grepruby.com>
2018-07-02 18:28:23 +0530

    adeded login tests

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -255,3 +255,56 @@ describe('POST /users', () => {
     .end(done)
   })
 });
+
+// POST /users/login
+
+describe('POST /users/login', () => {
+
+  it('should login and return the auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: users[1].email, password: users[1].password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => {
+          done(e);
+        })
+      });
+  });
+
+  it('it should return 400 if the login fails', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: users[1].email, password: users[1].password+'s'})
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => {
+          done(e);
+        })
+      });
+  });
+
+})
