ff5edbeecf236e08733c75e51486f54bd240e77e
Chanakya Devraj <cdevraj@grepruby.com>
2018-07-05 15:18:36 +0530

    added tests and utils

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -2,8 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message')
-const {generateLocationMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
--- node-chat-app/server/utils/message.test.js
+++ node-chat-app/server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMesssage', () => {
   it('should generate to correct message object', () => {
@@ -15,3 +15,19 @@ describe('generateMesssage', () => {
     expect(typeof result.createdAt).toBe('number');
   })
 })
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', ()=> {
+    var from = 'Admin';
+    var lat = 123;
+    var long = 324;
+    var url = `https://www.google.com/maps?q=${lat},${long}`
+
+    var message = generateLocationMessage(from, lat, long);
+    expect(message).toMatchObject({
+      from,
+      url
+    })
+    expect(typeof message.createdAt).toBe('number');
+  });
+});
